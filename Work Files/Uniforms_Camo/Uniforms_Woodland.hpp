7b0a515407d5ec5cc6cb0016a2c482167208e6a8
DragonGurdian5 <jtkalphabravo123@gmail.com>
2022-06-23 22:19:14 -0400

    Added Woodland Uniforms

--- Work Files/Uniforms_Camo/Uniforms_Woodland.hpp
+++ Work Files/Uniforms_Camo/Uniforms_Woodland.hpp
@@ -1,9 +1,9 @@
 class CfgPatches
 {
-	class ZDN_Uniforms_Useless
+	class ZDN_Uniforms_Woodland
 	{
 		author  = "DragonGurdian5, Zackary29";
-		name    = "399th Camo Patterns - ACU";
+		name    = "399th Camo Patterns - Tigerstripe";
 		requiredAddons[]=
 		{
 			"A3_Characters_F",
@@ -12,23 +12,23 @@ class CfgPatches
 		requiredVersion=0.1;
 		weapons[]=
 		{
-			"ZDN_399_Vest_Base_Useless",
-			"ZDN_399_Vest_Command_Useless",
-			"ZDN_399_Vest_EngFull_Useless",
-			"ZDN_399_Vest_EngStripe_Useless",
-			"ZDN_399_Vest_Medic_Useless",
-			"ZDN_399_Vest_Troop_Base_Useless",
-			"ZDN_399_Vest_Troop_Command_Useless",
-			"ZDN_399_Vest_Troop_EngFull_Useless",
-			"ZDN_399_Vest_Troop_EngStripe_Useless",
-			"ZDN_399_Vest_Troop_Medic_Useless",
-			"ZDN_399_Vest_Troop_Two_Base_Useless",
-			"ZDN_399_Vest_Troop_Two_Command_Useless",
-			"ZDN_399_Vest_Troop_Two_EngFull_Useless",
-			"ZDN_399_Vest_Troop_Two_EngStripe_Useless",
-			"ZDN_399_Vest_Troop_Two_Medic_Useless",
-			"ZDN_399_Vest_Troop_Bandolier_Base_Useless",
-			"ZDN_399_Vest_Troop_Bandolier_Command_Useless"
+			"ZDN_399_Vest_Base_Woodland",
+			"ZDN_399_Vest_Command_Woodland",
+			"ZDN_399_Vest_EngFull_Woodland",
+			"ZDN_399_Vest_EngStripe_Woodland",
+			"ZDN_399_Vest_Medic_Woodland",
+			"ZDN_399_Vest_Troop_Base_Woodland",
+			"ZDN_399_Vest_Troop_Command_Woodland",
+			"ZDN_399_Vest_Troop_EngFull_Woodland",
+			"ZDN_399_Vest_Troop_EngStripe_Woodland",
+			"ZDN_399_Vest_Troop_Medic_Woodland",
+			"ZDN_399_Vest_Troop_Two_Base_Woodland",
+			"ZDN_399_Vest_Troop_Two_Command_Woodland",
+			"ZDN_399_Vest_Troop_Two_EngFull_Woodland",
+			"ZDN_399_Vest_Troop_Two_EngStripe_Woodland",
+			"ZDN_399_Vest_Troop_Two_Medic_Woodland",
+			"ZDN_399_Vest_Troop_Bandolier_Base_Woodland",
+			"ZDN_399_Vest_Troop_Bandolier_Command_Woodland"
 		};
 	};
 };
@@ -44,14 +44,14 @@ class CfgWeapons
 	class JMSST_troops_Agrade_troop2_MI;
 	class JMSST_troops_Agrade_troopBand_MI;
 
-    class ZDN_399_Vest_Base_Useless: JMSST_troops_Agrade_MI
+    class ZDN_399_Vest_Base_Woodland: JMSST_troops_Agrade_MI
     {
         author="Jmax (Originally), Zaigan (Retexture)";
 		_generalMacro="JMSST_troops_Agrade_MI";
 		scope=2;
 		side=3;
 		armor=2;
-        displayName="[399th MI] Infantry Vest (ACU) (Basic)";
+        displayName="[399th MI] Infantry Vest (Tigerstripe) (Basic)";
 		picture="\JMSST_troops\data\ico\ico_v_Agrade_ca.paa";
 		model="\JMSST_troops\vests\Agrade.p3d";
 		hiddenSelections[]=
@@ -60,7 +60,7 @@ class CfgWeapons
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Woodland.paa"
 		};
         class ItemInfo: VestItem
 		{
@@ -113,9 +113,9 @@ class CfgWeapons
 			};
 		};
 	};
-    class ZDN_399_Vest_Command_Useless: ZDN_399_Vest_Base_Useless
+    class ZDN_399_Vest_Command_Woodland: ZDN_399_Vest_Base_Woodland
     {
-        displayName="[399th MI] Command Vest (ACU) (Basic)";
+        displayName="[399th MI] Command Vest (Tigerstripe) (Basic)";
 		picture="\JMSST_troops\data\ico\ico_v_Agrade_ca.paa";
 		hiddenSelections[]=
 		{
@@ -123,12 +123,12 @@ class CfgWeapons
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Command_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Command_Woodland.paa"
 		};
     };
-    class ZDN_399_Vest_EngFull_Useless: ZDN_399_Vest_Base_Useless
+    class ZDN_399_Vest_EngFull_Woodland: ZDN_399_Vest_Base_Woodland
     {
-        displayName="[399th MI] Engineer Vest (ACU) (Full) (Basic)";
+        displayName="[399th MI] Engineer Vest (Tigerstripe) (Full) (Basic)";
 		picture="\JMSST_troops\data\ico\ico_v_Agrade_ca.paa";
 		hiddenSelections[]=
 		{
@@ -136,12 +136,12 @@ class CfgWeapons
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Useless_1.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Woodland_1.paa"
 		};
 	};
-    class ZDN_399_Vest_EngStripe_Useless: ZDN_399_Vest_Base_Useless
+    class ZDN_399_Vest_EngStripe_Woodland: ZDN_399_Vest_Base_Woodland
     {
-        displayName="[399th MI] Engineer Vest (ACU) (Stripe) (Basic)";
+        displayName="[399th MI] Engineer Vest (Tigerstripe) (Stripe) (Basic)";
 		picture="\JMSST_troops\data\ico\ico_v_Agrade_ca.paa";
 		hiddenSelections[]=
 		{
@@ -149,12 +149,12 @@ class CfgWeapons
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Useless_2.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Woodland_2.paa"
 		};
 	};
-    class ZDN_399_Vest_Medic_Useless: ZDN_399_Vest_Base_Useless
+    class ZDN_399_Vest_Medic_Woodland: ZDN_399_Vest_Base_Woodland
     {
-        displayName="[399th MI] Medic Vest (ACU) (Basic)";
+        displayName="[399th MI] Medic Vest (Tigerstripe) (Basic)";
 		picture="\JMSST_troops\data\ico\ico_v_Agrade_ca.paa";
 		hiddenSelections[]=
 		{
@@ -162,18 +162,18 @@ class CfgWeapons
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Medical_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Medical_Woodland.paa"
 		};
 	};
 
-    class ZDN_399_Vest_Troop_Base_Useless: JMSST_troops_Agrade_troop_MI
+    class ZDN_399_Vest_Troop_Base_Woodland: JMSST_troops_Agrade_troop_MI
     {
         author="Jmax (Originally), Zaigan (Retexture)";
 		_generalMacro="JMSST_troops_Agrade_troop_MI";
 		scope=2;
 		side=3;
 		armor=2;
-        displayName="[399th MI] Infantry Vest (ACU) (Troop)";
+        displayName="[399th MI] Infantry Vest (Tigerstripe) (Troop)";
 		picture="\JMSST_troops\data\ico\ico_v_Agrade_ca.paa";
 		model="\JMSST_troops\vests\Agrade_troop.p3d";
 		hiddenSelections[]=
@@ -182,7 +182,7 @@ class CfgWeapons
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Woodland.paa"
 		};
         class ItemInfo: VestItem
 		{
@@ -235,63 +235,63 @@ class CfgWeapons
 			};
 		};
 	};
-    class ZDN_399_Vest_Troop_Command_Useless: ZDN_399_Vest_Troop_Base_Useless
+    class ZDN_399_Vest_Troop_Command_Woodland: ZDN_399_Vest_Troop_Base_Woodland
     {
-        displayName="[399th MI] Command Vest (ACU) (Troop)";
+        displayName="[399th MI] Command Vest (Tigerstripe) (Troop)";
 		hiddenSelections[]=
 		{
 			"Camo1"
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Command_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Command_Woodland.paa"
 		};
     };
-    class ZDN_399_Vest_Troop_EngFull_Useless: ZDN_399_Vest_Troop_Base_Useless
+    class ZDN_399_Vest_Troop_EngFull_Woodland: ZDN_399_Vest_Troop_Base_Woodland
     {
-        displayName="[399th MI] Engineer Vest (ACU) (Full) (Troop)";
+        displayName="[399th MI] Engineer Vest (Tigerstripe) (Full) (Troop)";
 		hiddenSelections[]=
 		{
 			"Camo1"
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Useless_1.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Woodland_1.paa"
 		};
     };
-    class ZDN_399_Vest_Troop_EngStripe_Useless: ZDN_399_Vest_Troop_Base_Useless
+    class ZDN_399_Vest_Troop_EngStripe_Woodland: ZDN_399_Vest_Troop_Base_Woodland
     {
-        displayName="[399th MI] Engineer Vest (ACU) (Stripe) (Troop)";
+        displayName="[399th MI] Engineer Vest (Tigerstripe) (Stripe) (Troop)";
 		hiddenSelections[]=
 		{
 			"Camo1"
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Useless_2.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Woodland_2.paa"
 		};
     };
-    class ZDN_399_Vest_Troop_Medic_Useless: ZDN_399_Vest_Troop_Base_Useless
+    class ZDN_399_Vest_Troop_Medic_Woodland: ZDN_399_Vest_Troop_Base_Woodland
     {
-        displayName="[399th MI] Medic Vest (ACU) (Troop)";
+        displayName="[399th MI] Medic Vest (Tigerstripe) (Troop)";
 		hiddenSelections[]=
 		{
 			"Camo1"
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Medical_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Medical_Woodland.paa"
 		};
     };
 
-    class ZDN_399_Vest_Troop_Two_Base_Useless: JMSST_troops_Agrade_troop2_MI
+    class ZDN_399_Vest_Troop_Two_Base_Woodland: JMSST_troops_Agrade_troop2_MI
     {
         author="Jmax (Originally), Zaigan (Retexture)";
 		_generalMacro="JMSST_troops_Agrade_troop2_MI";
 		scope=2;
 		side=3;
 		armor=2;
-        displayName="[399th MI] Infantry Vest (ACU) (Troop 2)";
+        displayName="[399th MI] Infantry Vest (Tigerstripe) (Troop 2)";
 		picture="\JMSST_troops\data\ico\ico_v_Agrade_ca.paa";
 		model="\JMSST_troops\vests\Agrade_troop2.p3d";
 		hiddenSelections[]=
@@ -300,7 +300,7 @@ class CfgWeapons
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Woodland.paa"
 		};
         class ItemInfo: VestItem
 		{
@@ -353,63 +353,63 @@ class CfgWeapons
 			};
 		};
 	};
-    class ZDN_399_Vest_Troop_Two_Command_Useless: ZDN_399_Vest_Troop_Base_Useless
+    class ZDN_399_Vest_Troop_Two_Command_Woodland: ZDN_399_Vest_Troop_Base_Woodland
     {
-        displayName="[399th MI] Command Vest (ACU) (Troop 2)";
+        displayName="[399th MI] Command Vest (Tigerstripe) (Troop 2)";
 		hiddenSelections[]=
 		{
 			"Camo1"
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Command_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Command_Woodland.paa"
 		};
     };
-    class ZDN_399_Vest_Troop_Two_EngFull_Useless: ZDN_399_Vest_Troop_Base_Useless
+    class ZDN_399_Vest_Troop_Two_EngFull_Woodland: ZDN_399_Vest_Troop_Base_Woodland
     {
-        displayName="[399th MI] Engineer Vest (ACU) (Full) (Troop 2)";
+        displayName="[399th MI] Engineer Vest (Tigerstripe) (Full) (Troop 2)";
 		hiddenSelections[]=
 		{
 			"Camo1"
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Useless_1.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Woodland_1.paa"
 		};
     };
-    class ZDN_399_Vest_Troop_Two_EngStripe_Useless: ZDN_399_Vest_Troop_Base_Useless
+    class ZDN_399_Vest_Troop_Two_EngStripe_Woodland: ZDN_399_Vest_Troop_Base_Woodland
     {
-        displayName="[399th MI] Engineer Vest (ACU) (Stripe) (Troop 2)";
+        displayName="[399th MI] Engineer Vest (Tigerstripe) (Stripe) (Troop 2)";
 		hiddenSelections[]=
 		{
 			"Camo1"
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Useless_2.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Eng_Vest_Woodland_2.paa"
 		};
     };
-    class ZDN_399_Vest_Troop_Two_Medic_Useless: ZDN_399_Vest_Troop_Base_Useless
+    class ZDN_399_Vest_Troop_Two_Medic_Woodland: ZDN_399_Vest_Troop_Base_Woodland
     {
-        displayName="[399th MI] Medic Vest (ACU) (Troop 2)";
+        displayName="[399th MI] Medic Vest (Tigerstripe) (Troop 2)";
 		hiddenSelections[]=
 		{
 			"Camo1"
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Medical_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Medical_Woodland.paa"
 		};
     };
 
-    class ZDN_399_Vest_Troop_Bandolier_Base_Useless: JMSST_troops_Agrade_troopBand_MI
+    class ZDN_399_Vest_Troop_Bandolier_Base_Woodland: JMSST_troops_Agrade_troopBand_MI
 	{
 		author="Jmax (Originally), Zaigan (Retexture)";
 		_generalMacro="JMSST_troops_Agrade_troopBand_MI";
 		scope=2;
 		side=3;
 		armor=2;
-		displayName="[399th MI] Infantry Vest (ACU) (Troop-B)";
+		displayName="[399th MI] Infantry Vest (Tigerstripe) (Troop-B)";
 		picture="\JMSST_troops\data\ico\ico_v_Agrade_ca.paa";
 		model="\JMSST_troops\vests\Agrade_troop_band.p3d";
 		hiddenSelections[]=
@@ -418,7 +418,7 @@ class CfgWeapons
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Woodland.paa"
 		};
 		class ItemInfo: VestItem
 		{
@@ -471,16 +471,16 @@ class CfgWeapons
 			};
 		};
 	};
-    class ZDN_399_Vest_Troop_Bandolier_Command_Useless: ZDN_399_Vest_Troop_Bandolier_Base_Useless
+    class ZDN_399_Vest_Troop_Bandolier_Command_Woodland: ZDN_399_Vest_Troop_Bandolier_Base_Woodland
     {
-        displayName = "[399th MI] Command Vest (ACU) (Troop-B)";
+        displayName = "[399th MI] Command Vest (Tigerstripe) (Troop-B)";
         hiddenSelections[]=
 		{
 			"Camo1"
 		};
         hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\vest\ZDN_399_Vest_Command_Useless.paa"
+			"Uniforms_Camo\data\vest\ZDN_399_Vest_Command_Woodland.paa"
 		};
     };
 
@@ -490,12 +490,12 @@ class CfgWeapons
 
 	class JMSST_troops_M3_helmet;
 
-	class ZDN_399_Helmet_Base_Useless: JMSST_troops_M3_helmet
+	class ZDN_399_Helmet_Base_Woodland: JMSST_troops_M3_helmet
 	{
 		author="Jmax (Originally), Zaigan (Retexture)";
 		weaponPoolAvailable = 1;
 		scope=2;
-		displayName="[399th MI] Infantry Helmet (ACU)";
+		displayName="[399th MI] Infantry Helmet (Tigerstripe)";
 		picture="\JMSST_troops\data\ico\ico_h_M3.paa";
 		model="\jmsst_troops\helmets\m3helmet.p3d";
 		hiddenSelections[]=
@@ -504,17 +504,17 @@ class CfgWeapons
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\helmets\ZDN_399_Camo_M3_base_Useless_co.paa"
+			"Uniforms_Camo\data\helmets\ZDN_399_Camo_M3_base_Woodland_co.paa"
 		};
 		ace_hearing_protection = 1.00; // Protection against Deafening
 		ace_hearing_lowerVolume = 0.60; // Lowers Volume with Helmet On
 	};
-	class ZDN_399_Helmet_Medic_One_Useless: JMSST_troops_M3_helmet
+	class ZDN_399_Helmet_Medic_One_Woodland: JMSST_troops_M3_helmet
 	{
 		author="Jmax (Originally), Zaigan (Retexture)";
 		weaponPoolAvailable = 1;
 		scope=2;
-		displayName="[399th MI] Medic Helmet (ACU)";
+		displayName="[399th MI] Medic Helmet (Tigerstripe)";
 		picture="\JMSST_troops\data\ico\ico_h_M3.paa";
 		model="\jmsst_troops\helmets\m3helmet.p3d";
 		hiddenSelections[]=
@@ -523,17 +523,17 @@ class CfgWeapons
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\helmets\ZDN_399_Medical_Helmet_Useless.paa"
+			"Uniforms_Camo\data\helmets\ZDN_399_Medical_Helmet_Woodland.paa"
 		};
 		ace_hearing_protection = 1.00; // Protection against Deafening
 		ace_hearing_lowerVolume = 0.60; // Lowers Volume with Helmet On
 	};
-	class ZDN_399_Helmet_Medic_Two_Useless: JMSST_troops_M3_helmet
+	class ZDN_399_Helmet_Medic_Two_Woodland: JMSST_troops_M3_helmet
 	{
 		author="Jmax (Originally), Zaigan (Retexture)";
 		weaponPoolAvailable = 1;
 		scope=2;
-		displayName="[399th MI] Medic Helmet (ACU)";
+		displayName="[399th MI] Medic Helmet (Tigerstripe)";
 		picture="\JMSST_troops\data\ico\ico_h_M3.paa";
 		model="\jmsst_troops\helmets\m3helmet.p3d";
 		hiddenSelections[]=
@@ -542,17 +542,17 @@ class CfgWeapons
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\helmets\ZDN_399_Medical_Helmet_2_Useless.paa"
+			"Uniforms_Camo\data\helmets\ZDN_399_Medical_Helmet_2_Woodland.paa"
 		};
 		ace_hearing_protection = 1.00; // Protection against Deafening
 		ace_hearing_lowerVolume = 0.60; // Lowers Volume with Helmet On
 	};
-	class ZDN_399_Helmet_Engineer_Useless: JMSST_troops_M3_helmet
+	class ZDN_399_Helmet_Engineer_Woodland: JMSST_troops_M3_helmet
 	{
 		author="Jmax (Originally), Zaigan (Retexture)";
 		weaponPoolAvailable = 1;
 		scope=2;
-		displayName="[399th MI] Engineer Helmet (ACU)";
+		displayName="[399th MI] Engineer Helmet (Tigerstripe)";
 		picture="\JMSST_troops\data\ico\ico_h_M3.paa";
 		model="\jmsst_troops\helmets\m3helmet.p3d";
 		hiddenSelections[]=
@@ -561,7 +561,7 @@ class CfgWeapons
 		};
 		hiddenSelectionsTextures[]=
 		{
-			"Uniforms_Camo\data\helmets\ZDN_399_Engineer_Useless.paa"
+			"Uniforms_Camo\data\helmets\ZDN_399_Engineer_Woodland.paa"
 		};
 		ace_hearing_protection = 1.00; // Protection against Deafening
 		ace_hearing_lowerVolume = 0.60; // Lowers Volume with Helmet On
